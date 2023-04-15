041aea02fec1fc0009183af91e091d2b247060a6
Ercan SORMAZ <ercansormaz@gmail.com>
2023-04-15 05:12:50 +0300

    Gateway add

--- grpc-client/src/main/java/dev/ercan/poc/cloud/grpc/eureka/grpc/client/controller/HelloController.java
+++ grpc-client/src/main/java/dev/ercan/poc/cloud/grpc/eureka/grpc/client/controller/HelloController.java
@@ -2,6 +2,7 @@ package dev.ercan.poc.cloud.grpc.eureka.grpc.client.controller;
 
 import dev.ercan.poc.cloud.grpc.eureka.grpc.client.service.HelloServiceGrpcClient;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -13,11 +14,14 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/hello")
 public class HelloController {
 
+  @Value("${eureka.instance.instance-id}")
+  private String instanceId;
+
   private final HelloServiceGrpcClient helloServiceGrpcClient;
 
   @GetMapping
   public ResponseEntity<String> sayHello(@RequestParam String name) {
-    return ResponseEntity.ok(helloServiceGrpcClient.sayHello(name));
+    return ResponseEntity.ok(helloServiceGrpcClient.sayHello(name) + " over " + instanceId);
   }
 
 }
