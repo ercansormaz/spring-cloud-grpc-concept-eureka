b1b13c018e7ef45eb88cc9eb77fbc5b5554145fb
Ercan SORMAZ <ercansormaz@gmail.com>
2023-04-15 05:21:09 +0300

    Spring Boot and Spring Cloud update but not compatible with version 3 yet

---
+++ grpc-client/src/main/java/dev/ercan/poc/cloud/grpc/eureka/grpc/client/config/GrpcConfig.java
@@ -0,0 +1,19 @@
+package dev.ercan.poc.cloud.grpc.eureka.grpc.client.config;
+
+import org.springframework.boot.autoconfigure.ImportAutoConfiguration;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ImportAutoConfiguration({
+    net.devh.boot.grpc.client.autoconfigure.GrpcClientAutoConfiguration.class,
+    net.devh.boot.grpc.client.autoconfigure.GrpcClientMetricAutoConfiguration.class,
+    net.devh.boot.grpc.client.autoconfigure.GrpcClientHealthAutoConfiguration.class,
+    net.devh.boot.grpc.client.autoconfigure.GrpcClientSecurityAutoConfiguration.class,
+    net.devh.boot.grpc.client.autoconfigure.GrpcClientTraceAutoConfiguration.class,
+    net.devh.boot.grpc.client.autoconfigure.GrpcDiscoveryClientAutoConfiguration.class,
+    net.devh.boot.grpc.common.autoconfigure.GrpcCommonCodecAutoConfiguration.class,
+    net.devh.boot.grpc.common.autoconfigure.GrpcCommonTraceAutoConfiguration.class,
+}) // TODO will be removed after Spring Boot 3 support add
+public class GrpcConfig {
+
+}
